7123701f920fba6b82da89236e300b557d2f63c9
ErickGLopes <99217380+ErickGLopes@users.noreply.github.com>
2022-11-05 16:33:38 -0300

    Add files via upload

---
+++ Java Básico/Exercícios/DoWhile/src/dowhile/DoWhile.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dowhile;
+
+/**
+ *
+ * @author devshi
+ */
+public class DoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int c = 0;
+        do {
+            System.out.println("Cambalhota " + (c + 1));
+            c++;
+        } while(c < 4);
+    }
+    
+}
