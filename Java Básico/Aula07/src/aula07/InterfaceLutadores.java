9eeaf8b2139b4761a62b64ed947abc0e69180ee2
ErickGLopes <99217380+ErickGLopes@users.noreply.github.com>
2022-11-05 16:27:29 -0300

    Add files via upload

---
+++ Java Básico/Aula07/src/aula07/InterfaceLutadores.java
@@ -0,0 +1,9 @@
+package aula07;
+
+public interface InterfaceLutadores {
+    public abstract void apresentar();
+    public abstract void status(); 
+    public abstract void ganharLuta();
+    public abstract void perderLuta();
+    public abstract void empatarLuta();
+}
