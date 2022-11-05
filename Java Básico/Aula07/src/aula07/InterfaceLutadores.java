c7c8a1cdb2202a1ec5799c156e61c3a5e70d2783
ErickGLopes <99217380+ErickGLopes@users.noreply.github.com>
2022-11-05 17:04:16 -0300

    Delete Java Básico/Aula07 directory

--- Java Básico/Aula07/src/aula07/InterfaceLutadores.java
+++
@@ -1,9 +0,0 @@
-package aula07;
-
-public interface InterfaceLutadores {
-    public abstract void apresentar();
-    public abstract void status(); 
-    public abstract void ganharLuta();
-    public abstract void perderLuta();
-    public abstract void empatarLuta();
-}
